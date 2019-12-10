4aabb56c8bcf49a680fb7955c20b2a1f805c677a
Victor Quiroz Castro <victorhqc@gmail.com>
2019-12-10 20:27:11 +0100

    ci: configure pipeline

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['.+\\.d\\.ts?$'],
+};
